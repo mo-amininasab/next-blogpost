644bff2e2888c7478fcae3df313c5b957432a144
mohammad_amini_nasab <mammadkingall@yahoo.com>
2021-10-09 16:08:45 +0330

    index->getStaticProps

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,48 +2,26 @@ import React, { Fragment } from 'react';
 import Hero from '../components/home-page/hero';
 import FeaturedPosts from '../components/home-page/featured-posts';
 
-const DUMMY_POSTS = [
-  {
-    slug: 'getting-started-with-nextjs1',
-    title: 'Getting Started with NextJS',
-    image: 'getting-started-nextjs.png',
-    excerpt:
-      'NextJs is a React framework for production - it makes building fullstack React apps and sites a breeze and ships with built-in SSR.',
-    data: '2022-02-10',
-  },
-  {
-    slug: 'getting-started-with-nextjs2',
-    title: 'Getting Started with NextJS',
-    image: 'getting-started-nextjs.png',
-    excerpt:
-      'NextJs is a React framework for production - it makes building fullstack React apps and sites a breeze and ships with built-in SSR.',
-    data: '2022-02-10',
-  },
-  {
-    slug: 'getting-started-with-nextjs3',
-    title: 'Getting Started with NextJS',
-    image: 'getting-started-nextjs.png',
-    excerpt:
-      'NextJs is a React framework for production - it makes building fullstack React apps and sites a breeze and ships with built-in SSR.',
-    data: '2022-02-10',
-  },
-  {
-    slug: 'getting-started-with-nextjs4',
-    title: 'Getting Started with NextJS',
-    image: 'getting-started-nextjs.png',
-    excerpt:
-      'NextJs is a React framework for production - it makes building fullstack React apps and sites a breeze and ships with built-in SSR.',
-    data: '2022-02-10',
-  },
-];
+import { getFeaturedPosts } from '../lib/posts.util';
 
-const HomePage = () => {
+const HomePage = ({ posts }) => {
   return (
     <Fragment>
       <Hero />
-      <FeaturedPosts posts={DUMMY_POSTS} />
+      <FeaturedPosts posts={posts} />
     </Fragment>
   );
 };
 
 export default HomePage;
+
+export function getStaticProps() {
+  const featuredPosts = getFeaturedPosts();
+
+  return {
+    props: {
+      posts: featuredPosts,
+    },
+    // revalidate: 3600 // no need
+  };
+}
