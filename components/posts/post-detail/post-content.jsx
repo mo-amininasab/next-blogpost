3e074932e8c167568699e08242d9630dc37f8efe
mohammad_amini_nasab <mammadkingall@yahoo.com>
2021-10-09 17:19:14 +0330

    image not render

--- components/posts/post-detail/post-content.jsx
+++ components/posts/post-detail/post-content.jsx
@@ -1,15 +1,51 @@
 import React from 'react';
 import ReactMarkdown from 'react-markdown';
+import Image from 'next/image';
 import PostHeader from './post-header';
 
 import classes from './post-content.module.css';
 
 const PostContent = ({ post }) => {
   const imagePath = `/images/posts/${post.slug}/${post.image}`;
+
+  const customRenderers = {
+    image(image) {
+      return (
+        <Image
+          src={`/images/posts/${post.slug}/${image.src}`} // ! bug here
+          alt={image.alt}
+          width={600}
+          height={300}
+        />
+      );
+    },
+    // alternative way
+    // paragraph(paragraph) {
+    //   const { node } = paragraph;
+
+    //   if (node.children[0].type === 'image') {
+    //     const image = node.children[0];
+
+    //     return (
+    //       <div className={classes.image}>
+    //         <Image
+    //           src={`/images/posts/${post.slug}/${image.url}`}
+    //           alt={image.alt}
+    //           width={600}
+    //           height={300}
+    //         />
+    //       </div>
+    //     );
+    //   }
+
+    //   return <p>{paragraph.children}</p>;
+    // },
+  };
+
   return (
     <article className={classes.content}>
       <PostHeader title={post.title} image={imagePath} />
-      <ReactMarkdown>{post.content}</ReactMarkdown>
+      <ReactMarkdown renderers={customRenderers}>{post.content}</ReactMarkdown>
     </article>
   );
 };
