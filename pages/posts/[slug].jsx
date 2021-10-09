1a8052dab8bcc8e536f2ba98385ac35bc90ddc6a
mohammad_amini_nasab <mammadkingall@yahoo.com>
2021-10-09 16:52:50 +0330

    PostDetailPage->getStaticProps

--- pages/posts/[slug].jsx
+++ pages/posts/[slug].jsx
@@ -1,10 +1,32 @@
-import React from 'react'
-import PostContent from '../../components/posts/post-detail/post-content'
+import React from 'react';
+import PostContent from '../../components/posts/post-detail/post-content';
 
-const PostDetailPage = () => {
-  return (
-    <PostContent />
-  )
+import { getPostData, getPostsFiles } from '../../lib/posts.util';
+
+const PostDetailPage = ({ post }) => {
+  return <PostContent post={post}/>;
+};
+
+export default PostDetailPage;
+
+export function getStaticProps({ params: { slug } }) {
+  const postData = getPostData(slug);
+
+  return {
+    props: {
+      post: postData,
+    },
+    revalidate: 600, // ! not slow down
+  };
 }
 
-export default PostDetailPage
+export function getStaticPaths() {
+  const postFileNames = getPostsFiles();
+
+  const slugs = postFileNames.map((fileName) => fileName.replace(/\.md$/, ''));
+
+  return {
+    paths: slugs.map((slug) => ({ params: { slug: slug } })),
+    fallback: 'blocking', // ? dont understand -> maximilian-e16
+  };
+}
